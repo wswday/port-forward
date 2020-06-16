d8e98643e953d62b5d001eebaa44cb7725960b94
wswday <wswday@sina.com>
2020-06-16 12:54:46 +0800

    optimize exception handling

--- forward.py
+++ forward.py
@@ -26,13 +26,16 @@ def parse_args(args):
     return settings
 
 
-def server(address, address_dst):
-    try:
-        dock_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        dock_socket.bind(address)
-        dock_socket.listen(5)
-        print('Forward {} -> {} succeed!'.format(address, address_dst))
-        while True:
+def server(address, address_dst, stop_service):
+    dock_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    dock_socket.bind(address)
+    dock_socket.listen(5)
+    dock_socket.settimeout(2)
+
+    stop_events = []
+    print('Forward {} -> {} succeed!'.format(address, address_dst))
+    while True:
+        try:
             socket_src, addr = dock_socket.accept()
             print('accept', addr)
 
@@ -40,27 +43,35 @@ def server(address, address_dst):
             socket_dst.connect(address_dst)
 
             stop_event = threading.Event()
+            stop_events.append(stop_event)
             threading.Thread(target=forward, args=('forward', socket_src, socket_dst, stop_event)).start()
             threading.Thread(target=forward, args=('backword', socket_dst, socket_src, stop_event)).start()
-    except Exception as e:
-        print('service', address, '->', address_dst, 'stop:', repr(e))
+        except socket.timeout:
+            if stop_service.is_set():
+                message = 'Stop by main thread'
+                break
+        except Exception as e:
+            message = repr(e)
+            break
+    for e in stop_events:
+        e.set()
+    print('service', address, '->', address_dst, 'stop:', message)
 
 
 def forward(direction, source, destination, stop_event):
-    source.settimeout(10)
-    destination.settimeout(10)
+    source.settimeout(2)
+    destination.settimeout(2)
     message = ''
     while True:
         try:
             buf = source.recv(4096)
             # print(len(buf))
-
-            if buf:
-                destination.sendall(buf)
-            else:
+                
+            if (not buf) or stop_event.is_set():
                 source.shutdown(socket.SHUT_RD)
                 destination.shutdown(socket.SHUT_WR)
                 break
+            destination.sendall(buf)
         except socket.timeout:
             if stop_event.is_set():
                 message = 'Stop by peer thread'
@@ -68,8 +79,8 @@ def forward(direction, source, destination, stop_event):
         except Exception as e:
             message = repr(e)
             break
-    print(direction, 'stop:', message)
     stop_event.set()
+    print(direction, 'stop:', message)
 
 
 if __name__ == '__main__':
@@ -96,10 +107,16 @@ if __name__ == '__main__':
 
     settings = parse_args(args) if len(args) > 0 else parse(config_file)
 
-    for setting in settings:
-        t = threading.Thread(target=server, args=setting)
-        t.start()
+    try:
+        stop_service = threading.Event()
+        for setting in settings:
+            t = threading.Thread(target=server, args=(*setting, stop_service))
+            t.start()
 
-    # wait for <ctrl-c>
-    while True:
-        time.sleep(60)
+        # wait for <ctrl-c>
+        while True:
+            time.sleep(60)
+    except Exception as e:
+        print('main:', repr(e))
+    finally:
+        stop_service.set()
